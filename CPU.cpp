6a6fc5bed61ca2d130ba8aaebd68c741be22379a
aidanvidal <aidan.w.vidal@gmail.com>
2025-04-07 19:43:18 -0400

    First Draft of cpu update cycles

---
+++ CPU.cpp
@@ -0,0 +1,29 @@
+#include "APU/APU.h"
+#include "Timers.h"
+
+/*--------------CPU Simulation-------------*/
+
+int handleInstruction() {
+  return 4; // Return the number of cycles for the instruction executed
+}
+
+void cpuUpdate(APU &apu, Timers &timers) {
+  // CPU Counters
+  const int MAXCYCLES = 69905;
+  int cyclesThisUpdate = 0;
+
+  while (cyclesThisUpdate < MAXCYCLES) {
+    int cycles = handleInstruction();
+    cyclesThisUpdate += cycles;
+    /*-----------APU Controls--------------*/
+    apu.updateChannelTimers(cycles);
+    apu.apuStep(cycles);
+    apu.getAudioSample(cycles);
+
+    /*-----------Timer Controls------------*/
+    // Update the timers
+    timers.updateTimers(cycles);
+  }
+}
+
+int main()
