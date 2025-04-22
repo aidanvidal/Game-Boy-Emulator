6eac94ecb6c56d845155e5a392fdd29a100ae0e4
aidanvidal <aidan.w.vidal@gmail.com>
2025-04-22 17:50:28 -0400

    Added some things

--- Cartridges/Cartridge.h
+++ Cartridges/Cartridge.h
@@ -8,10 +8,18 @@
 typedef unsigned char BYTE;
 typedef unsigned short WORD;
 
+// This is a base class for the cartridge
+// It will be inherited by different MBC classes
+// such as NoMBC, MBC1, MBC2, etc.
+// Each MBC class will implement its own read and write functions
+// to handle the specific memory mapping and banking logic
+// for that MBC type
 class Cartridge {
 public:
   Cartridge();
   virtual ~Cartridge();
+  virtual BYTE readData(WORD address) const = 0;       // Pure virtual function
+  virtual void writeData(WORD address, BYTE data) = 0; // Pure virtual function
 };
 
 #endif
